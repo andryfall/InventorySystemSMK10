e9a540be3eba6776acbf4a2eb9f1efc34622bf86
andryfall <andryfall@student.telkomuniversity.ac.id>
2025-02-17 13:32:17 +0700

    add LoginAPI

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,8 +17,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -34,15 +32,44 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Override the method to define the authentication field (username instead of email).
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName(): string
+    {
+        return 'username';
+    }
+
+    /**
+     * Override the method to retrieve the authentication identifier (username).
+     *
+     * @return string
+     */
+    public function getAuthIdentifier(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * Override the method to define the authentication password field.
+     *
+     * @return string
+     */
+    public function getAuthPassword(): string
+    {
+        return $this->password;
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Using the factory to create a user with specific data
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'username' => 'admin',   // Set username
+            'password' => \Illuminate\Support\Facades\Hash::make('pass123'), // Set hashed password
         ]);
     }
 }
